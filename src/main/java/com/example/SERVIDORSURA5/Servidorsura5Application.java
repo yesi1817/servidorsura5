73318ac78f0396ed0cb2c10ff37deaf415b75958
Your Name <you@example.com>
2024-09-09 11:55:58 -0500

    se crea proyecto

---
+++ src/main/java/com/example/SERVIDORSURA5/Servidorsura5Application.java
@@ -0,0 +1,13 @@
+package com.example.SERVIDORSURA5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servidorsura5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servidorsura5Application.class, args);
+	}
+
+}
